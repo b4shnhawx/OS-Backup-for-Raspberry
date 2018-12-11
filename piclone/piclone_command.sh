8409cd61748d1d478099e4f05ffcdc9a5dae3215
David Arribas Hidalgo <45437051+davidahid@users.noreply.github.com>
2018-12-11 23:46:09 +0100

    Delete piclone_command.sh

--- piclone/piclone_command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Saves the directory where is located the script.
-dir="$(cd "$(dirname "$0")" && pwd)"
-
-#Executes at the same time the scripts needed to work.
-bash $dir/piclone_lsblk_exe.sh
-bash $dir/piclone_piclone.sh
