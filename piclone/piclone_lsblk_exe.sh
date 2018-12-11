7238c0bc63ff80ebc95c2e274d3d8757b6211aff
David Arribas Hidalgo <45437051+davidahid@users.noreply.github.com>
2018-12-11 16:57:53 +0100

    Update piclone_lsblk_exe.sh

--- piclone/piclone_lsblk_exe.sh
+++ piclone/piclone_lsblk_exe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#Open new terminal with the output of the script.
+#This will execute the script that shows us the disks (.Piclone_lsblk.sh). The command open a new terminal with the output of the script.
 xfce4-terminal -H --geometry=+0+0 -e 'bash /home/pi/Scripts/piclone_lsblk.sh' &
 
