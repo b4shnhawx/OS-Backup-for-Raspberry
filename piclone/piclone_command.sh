08563b49a42d1c6e9b8945a23bb1d160604b478a
David Arribas Hidalgo <45437051+davidahid@users.noreply.github.com>
2018-12-11 16:34:14 +0100

    Update piclone_command.sh

--- piclone/piclone_command.sh
+++ piclone/piclone_command.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Saves the directory where is located the script.
+dir="$(cd "$(dirname "$0")" && pwd)"
+
 #Executes at the same time the scripts needed to work.
-bash /home/pi/Scripts/.Piclone_lsblk_exe.sh
-bash /home/pi/Scripts/.Piclone_piclone.sh
+bash $dir/.Piclone_lsblk_exe.sh
+bash $dir/.Piclone_piclone.sh
