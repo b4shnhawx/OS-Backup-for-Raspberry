4ae763b0fafe87fb16dfc5499d93a95cd747f1cb
David Arribas Hidalgo <45437051+davidahid@users.noreply.github.com>
2018-12-11 16:27:48 +0100

    Update piclone_lsblk.sh

--- piclone/piclone_lsblk.sh
+++ piclone/piclone_lsblk.sh
@@ -1,80 +1,74 @@
 #!/bin/bash
 
-#IMPORTANTE
-#Este script es muy concreto. Un ligero cambio en el sistema (cambiar el tamano de la SD de backup por ejemplo), podria variar el
-#comportamiento de este script ya que se fijan algunas unidades de almacenamiento. Tambien la salida puede quedar descolocada en
-#algunos casos, pero por ahora esta ok.
-#Esta por ver como se comporta al anadir nuevas unidades USB, remover otras, etc, pero a priori con esto no deberia presentar probelma.
-#Seguiremos informando.
+#IMPORTANT
+#This script its very concrete. A little change on it can vary the full behaviour of this script and show you a different disk in
+#which you want to make the backup.  Remember that we want to work with disks, and one error can suppose the lost of data.
 
-#-------------------------------------------------------------- Configuration --------------------------------------------------------------
+#----------------------------------------------------------- Configuration -----------------------------------------------------------
 
-#Creamos una variable en la que especificamos el tamanio del disco donde queremos hacer la copia de seguridad.
-#Colocamos el tamanio ya que es la forma mas facil de identificar dos discos con el lsblk, ya que el resto de valores puede cambiar.
-#IMPORTANTE
-#Si tenemos mas de un disco del mismo tamanio deberemos tener cuidado y corroborar que es correcto.
+#Create a variable which gonna have the destinatios disk size. #We put the size since it is the simplest method to identify two disks
+#with lsblk, since the rest of values can vary.
+#IMPORTANT
+#If we have more than one disk with the same size, we need to be carefull and make sure which is the #correct disk.
 backup_size="14.9G"
 
-#Tambien deberemos insertar el UUID del disco (este ID es unico en cada disco). De esta manera podemos diferenciar entre dos discos de tamanios
-#iguales. Aunque el script funcionaria solo con este ID, es mejor dejar dos valores por asegurar...
+#Also we may need to insert the UUID of the destination disk (this ID is exclusive for every disk). #In this way we can difference
+#between two disk of same sizes. Although the script could only work with this ID, it is better to set two values to make sure the
+#disk is correct...
 backup_uuid="25765c7c-5d3f-488a-85bc-bcb66bfa7c78"
 
-#------------------------------------------------------------- Script -------------------------------------------------------------
+#-------------------------------------------------------------- Script ---------------------------------------------------------------
 
-#Buscamos el ID del disco y lo guardamos en la variable blkid_backup_uuid. De esta manera si no encontrase el ID (disco no coenctado), la variable
-#estaria vacia.
+#Search the ID of the disk and we save it in the blkid_backup_uuid variable. In this way, if theres no ID available (the destination
+#disk its not connected), the variable will be empty.
 blkid_backup_uuid=`sudo blkid | grep -o $backup_uuid | uniq`
 
-#Guardamos la salida de lsblk en la ruta temporal de la raspberry (/temp)
+#Save te output of the command lsblk in the temporary path of the raspberry (/temp)
 lsblk > /tmp/lsblk_tmp_file
 
-#Contamos y guardamos el numero de lineas que hay en el lsblk del archivo creado en /temp
+#Counts and saves in the variable the number of lines that have the file lsblk_tmp_file
 linesnumber=`sudo cat /tmp/lsblk_tmp_file | grep disk | wc -l`
 
-#Creamos un bucle que se repite tantas veces como lineas tenga el lsblk
+#Create a bucle that repeats as many times as lines have the temporary file
 for line in $(seq 1 $linesnumber);
 do
-	#Guardamos el nombre del ultimo disco encontrado
+	#Saves the name of the last disk found
 	namedisk=`sudo cat /tmp/lsblk_tmp_file | grep disk | head -n $line | tail -n 1 | tr -s " " | cut -f1 -d" " | tr " " "\t"`
-	#Guardamos el tamaño del ultimo disco encontrado
+	#Saves the size of the last disk found
 	sizedisk=`sudo cat /tmp/lsblk_tmp_file | grep disk | head -n $line | tail -n 1 | tr -s " " | cut -f4 -d" " | tr " " "\t"`
-	#Guardamos el tipo del ultimo disco encontrado
+	#Saves the type of the last disk found
 	typedisk=`sudo cat /tmp/lsblk_tmp_file | grep disk | head -n $line | tail -n 1 | tr -s " " | cut -f6 -d" " | tr " " "\t"`
-	#Guardamos el punto de montaje del ultimo disco encontrado
+	#Saves the mount point of the last disk found
 	mountdisk=`sudo cat /tmp/lsblk_tmp_file | grep part | head -n $line | tail -n 1 | tr -s " " | cut -f7-8 -d" " | tr " " "\t"`
 
-	#Si el nombre del disco es igual a sd* (sda, sdb, sdc, etc), y el tamano es igual a 14.9G (SD de backup que estoy usando)...
+	#If the name disk is equal to sd* (sda, sdb, sdc, etc), the size is equal to 14.9G (depends of the variable) and the UUID is
+	#equal to the variable uuid...
 	if [[ $namedisk == sd* && $backup_uuid = $blkid_backup_uuid && $sizedisk = $backup_size ]];
 	then
-		#...imprime en pantalla el nombre (subrayado en verde [42m y con letra blanca [30m), tamanio, tipo y punto de anclaje dee
-		#la SD de backup. Aqui es donde se debe hacer el backup.
+		#...print the name (underlined in green [42m and with white letters [30m), size, type and the mount point of the
+		#destination disk. Here is where we gonna make the backup.
 		echo -e  "\e[42m\e[30m"$namedisk"\e[0m\e[0m" "\t" $sizedisk "\t\t" $typedisk "\t" $mountdisk
 
-	#...si el nombre del disco es igual a sd* (sda, sdb, sdc, etc), y el tamano es igual a 931.5G (NAS)...
+	#If the name of the disk its another sd* (sda, sdb, sdc, etc) that doesnt coincided withe the first condition (name,
+	#uuid and size)...
 	elif [[ $namedisk == sd* ]];
 	then
-		#...imprime en pantalla el nombre (subrayado en rojo [41m y con letra blanca [30m), tamaño, tipo y punto de anclaje del
-		#NAS. AQUI NO SE DEBE HACER LA COPIA.
+		#...print the name (underlined in red [41m and with white letters [30m), size, type and the mount point of the
+		#destination disk. This are the disks that we dont touch. IN THIS DEVICES WE CAN'T MAKE THE BACKUP.
 		echo -e "\e[41m\e[30m"$namedisk"\e[0m\e[0m" "\t" $sizedisk "\t" $typedisk "\t" "\e[41m\e[30m"$mountdisk"\e[0m\e[0m"
 
-	#...si el nombre del disco es igual a mmcblk0, es decir donde se encuentra el OS (lo pongo a parte ya que no se epuede tabular entre
-	#$namedisk y $sizedisk ya que el nombre es muy largo y desordena la salida, por lo que hay que tratarlo diferente y poner nada mas
-	#que un espacio, pero no lo pongo ahi por otra cosa)...
-	elif [[ $namedisk = "mmcblk0" || $blkid_backup_uuid != $backup_uuid || $blkid_dont_touch_uuid != $dont_touch_uuid ]];
+	#If the name disk is equal to mmcblk0 (where is the OS located)...
+	elif [[ $namedisk = "mmcblk0" ]];
 	then
-		#...imprime en pantalla el nombre, tamaño, tipo y punto de anclaje de los archivos del OS. AQUI NO SE DEBE HACER LA COPIA
-		#aunque de todas formas no deja hacerla xd
+		#...print the name, size, type and mount point of the disk where are located the OS files.
 		echo -e $namedisk "" $sizedisk "\t\t" $typedisk "\t" $mountdisk
 
-	#...y si no coincide con ninugno de los anteriores, es decir, que son unidades de almacenamtiento nuevas...
+	#And if no condition is met, it means that there are other disk connected...
 	else
-		#...imprime en pantalla el nombre, tamaño, tipo y punto de anclaje de los archivos de la unidad.
+		#...print the name, sizez type amd mount point of the disk.
 		echo -e $namedisk "\t" $sizedisk "\t" $typedisk "\t" $mountdisk
 
 	fi
-#Termina el bucle y pasamos al siguiente disco que haya en el lsblk. Si no hay mas el script termina.
-done
 
-#Pruebas
-#sudo cat /tmp/lsblk_tmp_file | grep disk | tr -s " " | cut -f1,4,6,8 -d" " | tr " " '\t''\t'
-#sudo cat /tmp/lsblk_tmp_file | tr "├─" " " | tr -s " " | cut -f1,2,5-8 -d" " | tr " " "     "
+#Finish the loop and move on to the next disk in the lsblk. If there is no more, the script ends.
+done
